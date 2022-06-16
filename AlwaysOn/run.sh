665ae0ea6b88afeb32f13bc1eff6ff6831d88522
hbsagen <48381155+hbsagen@users.noreply.github.com>
2022-06-16 19:06:23 +0200

    Update run.sh

--- AlwaysOn/run.sh
+++ AlwaysOn/run.sh
@@ -11,7 +11,7 @@ $SDPath/_utils/showScreen $SDPath/_screens/scriptStart.png
 mount -uw /mnt/efs-system
 mount -uw /mnt/nav
 
-#cp -V -r -f $SDPath/nav/run_gemmi.sh /mnt/nav/gemmi/ >> "$SDPath/injection"
+cp -V -r /mnt/nav/gemmi/run_gemmi.sh $SDPath/
 
 df / >> "$SDPath/paths"
 

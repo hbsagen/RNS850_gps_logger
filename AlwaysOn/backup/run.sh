c3564749700a7077a6b0e9c686b8f78fa41442c0
hbsagen <48381155+hbsagen@users.noreply.github.com>
2022-06-16 19:25:25 +0200

    Add files via upload

---
+++ AlwaysOn/backup/run.sh
@@ -0,0 +1,18 @@
+sdcard=`ls /mnt|grep sdcard.*t`
+
+SDPath=/mnt/$sdcard
+
+mount -u $SDPath
+
+$SDPath/_utils/showScreen $SDPath/_screens/scriptStart.png
+
+#BoS ---------------------------------------
+
+mount -uw /mnt/efs-system
+mount -uw /mnt/nav
+
+cp -V -r -f $SDPath/backup/run_gemmi.sh /mnt/nav/gemmi/
+
+#EoS ------------------------------------------------------
+
+$SDPath/_utils/showScreen $SDPath/_screens/scriptDone.png
